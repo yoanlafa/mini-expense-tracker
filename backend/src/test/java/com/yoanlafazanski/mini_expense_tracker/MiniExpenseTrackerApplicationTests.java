d37acea6b8e5a2a22aa23efb5d72f857302e4d63
Yoan Lafazanski <yoanlafa@gmail.com>
2026-06-15 14:50:35 +0200

    first commit

---
+++ backend/src/test/java/com/yoanlafazanski/mini_expense_tracker/MiniExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoanlafazanski.mini_expense_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
